bbe54ad1a0f305eae00c3eca6362919447988286
ervnjmsdnts <earvinjamesdantes@gmail.com>
2021-11-29 00:45:32 +0800

    Refactored creating interests

--- server/app/features/interest/interest_models.py
+++ server/app/features/interest/interest_models.py
@@ -2,10 +2,15 @@ from typing import List, Optional
 from pydantic import BaseModel
 
 
+class InterestObject(BaseModel):
+    name: str
+    isSelected: Optional[bool] = False
+
+
 class Interest(BaseModel):
     category: str
-    interests: List[str]
+    interests: List[InterestObject] = None
 
 
 class InterestUpdate(BaseModel):
-    interests: Optional[str]
+    interests: Optional[InterestObject]
--- server/app/features/interest/interest_routes.py
+++ server/app/features/interest/interest_routes.py
@@ -4,7 +4,7 @@ from fastapi.param_functions import Header
 
 from app.core.db import interest_collection
 
-from .interest_models import Interest, InterestUpdate
+from .interest_models import Interest, InterestObject, InterestUpdate
 
 router = APIRouter(prefix="/interest", tags=["interest"])
 
@@ -31,22 +31,19 @@ async def createInterest(interest: Interest):
     if categoryExist:
         raise HTTPException(400, "Category already exist. Try to update it if you want to add more items")
 
-    await interest_collection.insert_one(interest_data)
+    await interest_collection.insert_one(interest.dict())
 
-    return {"message": "Interest created"}
+    return interest
 
 
 @router.put("/{category}")
 async def addInterest(category: str, interest: InterestUpdate):
-    interest_data = dict(interest)
     categoryExist = await interest_collection.find_one({"category": category})
 
     if not categoryExist:
         return {"message": "Can't find category"}
 
-    updated_interest = await interest_collection.update_one(
-        {"category": category}, {"$push": {"interests": interest_data["interests"]}}
-    )
+    updated_interest = await interest_collection.update_one({"category": category}, {"$push": interest.dict()})
 
     if updated_interest:
         return {"message": "Interest Updated"}
